7c6af4b0e32aaec3deecad9cba77ea9278287f8a
Luc Kabamba <klucakimana@yahoo.com>
2021-12-17 00:31:17 +0100

    made some change

--- kajsasalfabet/collection.swift
+++ kajsasalfabet/collection.swift
@@ -67,8 +67,7 @@ struct collection: View {
                     }
                     .frame(maxHeight: .infinity)
                     .navigationTitle("Alfabetet")
-//                    .navigationBarTitle(Text("Alfabetet")).navigationBarHidden(false).foregroundColor(Color("newColor"))
-                    
+
                     Button(action: {
                         hejdåbutton = true
                         
@@ -94,36 +93,7 @@ struct collection: View {
 //            .background(Color.red)
        }
     }
-    func complexSuccess() {
-        // make sure that the device supports haptics
-        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
-        var events = [CHHapticEvent]()
-        
-        for i in stride(from: 0, to: 1, by: 0.1) {
-            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: Float(i))
-            let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: Float(i))
-            let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: i)
-            events.append(event)
-        }
-
-        for i in stride(from: 0, to: 1, by: 0.1) {
-            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: Float(1 - i))
-            let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: Float(1 - i))
-            let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: 1 + i)
-            events.append(event)
-        }
-
-        // convert those events into a pattern and play it immediately
-        do {
-            let pattern = try CHHapticPattern(events: events, parameters: [])
-            let player = try engine?.makePlayer(with: pattern)
-            try player?.start(atTime: 0)
-        } catch {
-            print("Failed to play pattern: \(error.localizedDescription).")
-        }
-    }
-    }
-
+    
     
     struct collection_Previews: PreviewProvider {
         static var previews: some View {
@@ -171,12 +141,5 @@ class AllTexts {
         thetexts.append("Ögon och öron ömhet önskar.")
 
     }
-}
-extension View {
-    func hapticFeedback(style: UIImpactFeedbackGenerator.FeedbackStyle = .light) -> some View{ self.onTapGesture {
-        let impactHeavy = UIImpactFeedbackGenerator(style: .heavy)
-        impactHeavy.impactOccurred()
-        
-        }
-    }
+  }
 }
